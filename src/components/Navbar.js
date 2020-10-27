9529f593a49157c7e5f5cd2dfd58a883b8c4d21d
Phillip Nguyen <nh273@cornell.edu>
2020-10-27 19:34:09 +0100

    adding navlinks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Drawer from "@material-ui/core/Drawer";
-import Button from "@material-ui/core/Button";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import { NavLink } from "react-router-dom";
 
 export const Navbar = (props) => {
   const [drawerOpen, setDrawerOpen] = React.useState(false);
@@ -11,19 +15,31 @@ export const Navbar = (props) => {
     setDrawerOpen(open);
   };
 
+  const NavTo = ["/lesson-1", "lesson-2", "lesson-3", "lesson-4"];
+
   return (
     <React.Fragment>
-      <Button onClick={toggleDrawer(true)}>open drawer</Button>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton onClick={toggleDrawer(true)}>
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
       <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer(false)}>
         <div className="drawer-opened" onClick={toggleDrawer(false)}>
           <List>
-            {["Home", "Lessons", "About"].map((text, index) => {
-              return (
-                <ListItem button key={index}>
-                  <ListItemText primary={text} />
-                </ListItem>
-              );
-            })}
+            {["Lesson 1", "Lesson 2", "Lesson 3", "Lesson 4"].map(
+              (text, index) => {
+                return (
+                  <ListItem button key={index}>
+                    <NavLink to={NavTo[index]}>
+                      <ListItemText primary={text} />
+                    </NavLink>
+                  </ListItem>
+                );
+              }
+            )}
           </List>
         </div>
       </Drawer>
