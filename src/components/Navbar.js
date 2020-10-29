08b3dd61d9470e20f71fb8ea8ef7958d7bee71cd
Phillip Nguyen <nh273@cornell.edu>
2020-10-29 20:48:57 +0100

    Simplyfying home nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -82,7 +82,7 @@ export const Navbar = (props) => {
           <div className={classes.drawerHeader} />
           <Divider />
           <List>
-            <ListItem button key="home" component={Link} to="/">
+            <ListItem button onClick={(e) => handleListItemClick(e, 4)}>
               <ListItemText primary="Home" />
             </ListItem>
             <Divider />
