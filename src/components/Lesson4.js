70e5e632aa1a8e685a82a061f9639248b7f1ac23
Phillip Nguyen <nh273@cornell.edu>
2020-12-07 04:27:54 +0700

    some working version w stacked bar

--- src/components/Lesson4.js
+++ src/components/Lesson4.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Typography from "@material-ui/core/Typography";
+import { Scrollama, Step } from "react-scrollama";
 
 import LessonLayout from "./Lessons";
 import { StackedBar } from "../viz/StackedBar";
+import { stepStyle, chartStyle, StepContent } from "./Steps";
 
 export const Lesson4 = (props) => {
+  const [currentStep, setCurrentStep] = useState(0);
+  const onStepEnter = ({ data }) => {
+    setCurrentStep(data);
+  };
   return (
     <LessonLayout backLink="/error-bias" backText="Introduction to Errors">
       <Typography variant="h2" gutterBottom>
@@ -17,7 +23,55 @@ export const Lesson4 = (props) => {
       <Typography variant="body1" gutterBottom>
         But how do you know which treatment really works, and which doesn't?
       </Typography>
-      <StackedBar />
+      <div className="contains-sticky">
+        <div className="sticky" style={{ ...chartStyle, top: 200 }}>
+          <StackedBar currentStep={currentStep} />
+        </div>
+        <Scrollama onStepEnter={onStepEnter} offset={0.6}>
+          <Step data={0}>
+            <div className="step" style={stepStyle}>
+              <StepContent>
+                The key here is the piece of information on how rare this new
+                disease is. If you randomly select 100 persons, only 3 would
+                have the disease.
+              </StepContent>
+            </div>
+          </Step>
+
+          <Step data={1}>
+            <div className="step" style={stepStyle}>
+              <StepContent>
+                Even with our 99% accuracy, which in this situation we can take
+                to mean that we detected all 3 infected persons successfully,
+                overall you would still have only 3 people infected and
+                positive.
+              </StepContent>
+            </div>
+          </Step>
+
+          <Step data={3}>
+            <div className="step" style={stepStyle}>
+              <StepContent>
+                At 5% false positive, there will be about 5 people who are not
+                infected, but their tests are wrongly positive.
+              </StepContent>
+            </div>
+          </Step>
+
+          <Step data={4}>
+            <div className="step" style={stepStyle}>
+              <StepContent style={{ marginBottom: 500 }}>
+                So, you can see that even if you are among the 8 people who
+                tested positive, it is more likely that you are among the 5 who
+                were
+                <span style={{ color: "red" }}> misindentified </span> than the
+                3 who were{" "}
+                <span style={{ color: "green" }}> correctly identified </span>.
+              </StepContent>
+            </div>
+          </Step>
+        </Scrollama>
+      </div>
     </LessonLayout>
   );
 };
--- src/viz/StackedBar.js
+++ src/viz/StackedBar.js
@@ -7,37 +7,52 @@ import {
   VictoryStack,
 } from "victory";
 
+const data0 = [
+  { x: "Mortality of Treated patients", y: 0.0, fill: "#79C7E3" },
+  { x: "Mortality of Treated patients", y: 0.0, fill: "Tomato" },
+];
+const data1 = [
+  { x: "Mortality of Treated patients", y: 0.8, fill: "#79C7E3" },
+  { x: "Mortality of Treated patients", y: 0.2, fill: "Tomato" },
+];
+const dataDict = { 0: data0, 1: data1 };
 export const StackedBar = (props) => {
+  const data = dataDict[props.currentStep];
   const transparentStyle = {
     axis: { stroke: "transparent" },
     ticks: { stroke: "transparent" },
     tickLabels: { fill: "transparent" },
   };
   return (
-    <VictoryChart height={200} width={300} padding={50}>
+    <VictoryChart height={180} width={300} padding={25}>
       <VictoryAxis
         style={{
           axis: { stroke: "transparent" },
           ticks: { stroke: "transparent" },
-          tickLabels: { fontSize: 10 },
+          tickLabels: {
+            fontSize: 10,
+          },
         }}
       />
       <VictoryAxis style={transparentStyle} dependentAxis />
+
       <VictoryStack
-        style={{ data: { width: 20 }, labels: { fontSize: 8, fill: "white" } }}
+        style={{
+          data: { width: 20 },
+          labels: { fontSize: 8, fill: "white" },
+        }}
       >
-        <VictoryBar
-          style={{ data: { fill: "tomato" } }}
-          data={[{ x: "Mortality of Treated patients", y: 0.8 }]}
-          labels={({ datum }) => datum.y}
-          labelComponent={<VictoryLabel dy={15} />}
-        />
-        <VictoryBar
-          style={{ data: { fill: "#79C7E3" } }}
-          data={[{ x: "Mortality of Treated patients", y: 0.2 }]}
-          labels={({ datum }) => datum.y}
-          labelComponent={<VictoryLabel dy={15} />}
-        />
+        {(data || []).map((d, i) => {
+          return (
+            <VictoryBar
+              key={i}
+              style={{ data: { fill: d.fill } }}
+              data={[{ x: "Mortality of Treated patients", y: d.y }]}
+              labels={({ datum }) => datum.y}
+              labelComponent={<VictoryLabel dy={15} />}
+            />
+          );
+        })}
       </VictoryStack>
     </VictoryChart>
   );
