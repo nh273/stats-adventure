bfe59f7ac2ddafef17af723a861974d844132578
Phillip Nguyen <nh273@cornell.edu>
2020-12-07 02:20:38 +0700

    changing quiz answers. allow nav to lesson when quiz submitted

--- src/components/EntryQuiz.js
+++ src/components/EntryQuiz.js
@@ -6,53 +6,53 @@ const entryQuiz = [
   {
     qid: "entry_attitude_0",
     questionContent: "I like statistics.",
-    questionChoices: ["🤢", "😕", "😍"],
+    questionChoices: ["Disagree 🤢", "Neutral 😕", "Agree 😍"],
   },
   {
     qid: "entry_attitude_1",
     questionContent: "I am not afraid of statistics.",
-    questionChoices: ["🤢", "😕", "😍"],
+    questionChoices: ["Disagree 🤢", "Neutral 😕", "Agree 😍"],
   },
   {
     qid: "entry_attitude_2",
     questionContent: "I enjoy taking statistics courses.",
-    questionChoices: ["🤢", "😕", "😍"],
+    questionChoices: ["Disagree 🤢", "Neutral 😕", "Agree 😍"],
   },
   {
     qid: "entry_attitude_3",
     questionContent: "I am interested in learning statistics.",
-    questionChoices: ["🤢", "😕", "😍"],
+    questionChoices: ["Disagree 🤢", "Neutral 😕", "Agree 😍"],
   },
   {
     qid: "entry_attitude_4",
     questionContent: "I don’t find it hard to understand statistical concept.",
-    questionChoices: ["🤢", "😕", "😍"],
+    questionChoices: ["Disagree 🤢", "Neutral 😕", "Agree 😍"],
   },
   {
     qid: "entry_attitude_5",
     questionContent: "Statistics can be quickly learned by most people.",
-    questionChoices: ["🤢", "😕", "😍"],
+    questionChoices: ["Disagree 🤢", "Neutral 😕", "Agree 😍"],
   },
   {
     qid: "entry_attitude_6",
     questionContent: "I can learn statistics.",
-    questionChoices: ["🤢", "😕", "😍"],
+    questionChoices: ["Disagree 🤢", "Neutral 😕", "Agree 😍"],
   },
   {
     qid: "entry_attitude_7",
     questionContent: "Statistics should be a required part of my study",
-    questionChoices: ["🤢", "😕", "😍"],
+    questionChoices: ["Disagree 🤢", "Neutral 😕", "Agree 😍"],
   },
   {
     qid: "entry_attitude_8",
     questionContent:
       "Other than for study purpose, statistics is also useful for my daily life.",
-    questionChoices: ["🤢", "😕", "😍"],
+    questionChoices: ["Disagree 🤢", "Neutral 😕", "Agree 😍"],
   },
   {
     qid: "entry_attitude_9",
     questionContent: "I will use statistics in my future career.",
-    questionChoices: ["🤢", "😕", "😍"],
+    questionChoices: ["Disagree 🤢", "Neutral 😕", "Agree 😍"],
   },
 ];
 
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -5,8 +5,12 @@ import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CloudUploadIcon from "@material-ui/icons/CloudUpload";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
 import Typography from "@material-ui/core/Typography";
 import { withStyles } from "@material-ui/core/styles";
+import { Link } from "react-router-dom";
 import { Navbar } from "./Navbar";
 
 const styles = (theme) => ({
@@ -33,6 +37,9 @@ const styles = (theme) => ({
     alignItems: "center",
     paddingLeft: theme.spacing(1),
     paddingBottom: theme.spacing(1),
+  },
+  linkButton: {
+    textTransform: "none",
   },
 });
 
@@ -62,17 +69,46 @@ const Question = (props) => {
   );
 };
 
+const SubmitAlert = (props) => {
+  return (
+    <div>
+      <Dialog
+        open={props.open}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogContent>
+          Survey Submitted {props.error ? "unsuccessfully" : "successfully"}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={props.handleClose} color="primary">
+            Retry
+          </Button>
+          <Button
+            component={Link}
+            to="/descriptive-stats"
+            variant="contained"
+            color="primary"
+            autoFocus
+          >
+            Go to the first lesson!
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
 class Quiz extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       submitted: false,
+      alertOpen: false,
       answers: this.props.questions.map((q) => ({ ...q, value: null })),
     };
-    this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick(qid, value) {
+  handleClick = (qid, value) => {
     this.setState((state) => {
       return {
         answers: state.answers.map((q) => {
@@ -83,19 +119,29 @@ class Quiz extends React.Component {
         }),
       };
     });
-  }
+  };
 
-  handleSubmit() {
+  handleSuccess = () => this.setState({ submitted: false, alertOpen: true });
+  handleFail = () => this.setState({ submitted: true, alertOpen: true });
+
+  handleSubmit = () => {
     var dbref = this.props.dbref;
     var newSubmissionRef = dbref.push();
-    newSubmissionRef.set(this.state.answers, function (error) {
+    newSubmissionRef.set(this.state.answers, (error) => {
       if (error) {
-        alert(`There had been an error ${error}`);
+        alert(
+          `There had been an error ${error}. Consider contacting pnguyen320@gatech.edu`
+        );
+        this.handleFail();
       } else {
-        alert("Quiz submitted successfully");
+        this.handleSuccess();
       }
     });
-  }
+  };
+
+  handleCloseAlert = () => {
+    this.setState({ alertOpen: false });
+  };
 
   render() {
     const { classes } = this.props;
@@ -122,6 +168,10 @@ class Quiz extends React.Component {
             </CardActions>
           </Card>
         </div>
+        <SubmitAlert
+          open={this.state.alertOpen}
+          handleClose={this.handleCloseAlert}
+        />
       </React.Fragment>
     );
   }
